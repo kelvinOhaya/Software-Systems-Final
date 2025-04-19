374ab54475a7d7f843a5451ce06c061c8cb44c81
tsidibe135 <tsidibe135@gmail.com>
2025-04-19 00:24:45 -0400

    Update Main.java (#9)

--- Main.java
+++ Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Slotomainia slotMachine = new Slotomainia();
         slotMachine.run();
+        BlackJack j = new BlackJack();
+        j.run();
     }
 }
